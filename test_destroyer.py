38f69aec62d1bc0bd55db16dbbe72e2bfb28bc77
Maxim Odinokov <coder1@mail.ru>
2022-10-06 00:51:41 +0300

    code refactoring

--- test_destroyer.py
+++ test_destroyer.py
@@ -3,11 +3,17 @@ from traceless_destroyer import Destroyer
 
 
 class TestDestroyer(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.destroyer = Destroyer()
 
-    def test_fill(self):
-        self.assertEqual(self.destroyer.fill('test.txt', '$', 5), '$$$$$')
+    def test_01_fill(self):
+        self.assertEqual(self.destroyer.fill('D:\\test_pr3\\test.txt', 't', 3), 'ttt')
 
-    def test_destroyer(self):
-        self.assertEqual(self.destroyer.destroy('test.txt'), 'File has been destroyed!')
+    def test_02_destroyer(self):
+        self.assertEqual(self.destroyer.destroy('D:\\test_pr3\\test.txt'), 'File/directory has been destroyed!')
+        self.assertEqual(self.destroyer.destroy('D:\\test_pr3\\test_dir'), 'File/directory has been destroyed!')
+        self.assertEqual(self.destroyer.destroy('pr.txt'), 'File does not exist!')
+
+
+if __name__ == '__main__':
+    unittest.main()
