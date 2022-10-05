a87939bdbe1fbdcc5d234eff6cba0d96e3904ce4
Maxim Odinokov <coder1@mail.ru>
2022-10-05 22:41:33 +0300

    add new method 'test_destroyer 'in class TestDestroyer to test file destruction

--- test_destroyer.py
+++ test_destroyer.py
@@ -1,7 +1,13 @@
 import unittest
-import traceless_destroyer
+from traceless_destroyer import Destroyer
 
 
 class TestDestroyer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.destroyer = Destroyer()
+
     def test_fill(self):
-        self.assertEqual(traceless_destroyer.fill('$', 5), '$$$$$')
+        self.assertEqual(self.destroyer.fill('test.txt', '$', 5), '$$$$$')
+
+    def test_destroyer(self):
+        self.assertEqual(self.destroyer.destroy('test.txt'), 'File has been destroyed!')
